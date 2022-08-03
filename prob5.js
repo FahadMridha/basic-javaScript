24611d9eb4643eea1b82808acd726098929a87e4
fahadMridha <sohelcse16@gmail.com>
2022-08-03 17:55:21 +0600

    first commit

---
+++ prob5.js
@@ -0,0 +1,18 @@
+var num = [10, 30, 50, 34, 'Hello', 'Dhaka', 'Bangladesh'];
+// num[2] = 40;
+// console.log(num);
+
+// indexNum = num.indexOf('Hello');
+// console.log(indexNum);
+
+// num.pop()
+// console.log(num);
+
+// num.push('Bangladesh2');
+// console.log(num);
+
+// num.unshift('Noakhali');
+// console.log(num);
+
+// num.shift();
+// console.log(num);
